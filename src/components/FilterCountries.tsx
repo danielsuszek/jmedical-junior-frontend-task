36f25927b9a7685fbf9e2ead52edc2b693ec64bd
danielsuszek <danielsuszek1@gmail.com>
2022-03-12 14:13:52 +0100

    Add select-react

--- src/components/FilterCountries.tsx
+++ src/components/FilterCountries.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SelectContinentFilter from './SelectContinentFilter'
 
 const FilterCountries = () => {
   return (
     <div className="filterCountries">
-      FilterCountries
+      <SelectContinentFilter />
     </div>
   )
 }
---
+++ src/components/SelectContinentFilter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Select from 'react-select'
+
+const SelectContinentFilter = () => {
+  return (
+    <div>
+      SelectContinentFilter
+      <Select />
+    </div>
+  )
+}
+
+export default SelectContinentFilter
