eb57c0bb517b4fe0c9cb8729b17f8e851c20d7a7
danielsuszek <danielsuszek1@gmail.com>
2022-03-12 16:18:52 +0100

    Select continent setState works with mock data

--- src/components/SelectContinentFilter.tsx
+++ src/components/SelectContinentFilter.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import Select from 'react-select'
+import React, { FC, useState, FormEvent } from 'react'
+import Select, { OptionProps, Options } from 'react-select'
 
-const SelectContinentFilter = () => {
+
+
+
+
+interface Continent {
+  label: String;
+  value: String
+}
+
+const continents: Continent[] = [
+      {
+        "label": "Africa",
+        "value": "AF"
+      },
+      {
+        "label": "Antarctica",
+        "value": "AN"
+      },    
+    ]  
+
+// interface Continents {
+//   continents: Continent[]
+// }
+
+
+
+const SelectContinentFilter: FC = () => {
+  const [selectedContinent, setSelectedContinent] = useState({})
+  
+  console.log(selectedContinent);
+  const onChangeContinent = (option: Continent | null) => {
+    console.log(option);
+    setSelectedContinent((option) => option)
+
+  }
+  
   return (
     <div>
       SelectContinentFilter
-      <Select />
+      <Select<Continent>
+        options={continents}  
+        onChange={onChangeContinent}
+      />
     </div>
   )
 }
